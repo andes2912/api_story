67a6022bc1e3d6e8a48205bf60ee62276ce74e64
Andri Desmana <andridesmana29@gmail.com>
2023-05-23 16:55:57 +0700

    Refactor Auth

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,69 +3,33 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Services\AuthService;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rules\Password;
-use Illuminate\Support\Facades\Validator;
-use App\Models\User;
 use Auth;
 
 class AuthController extends Controller
 {
+    protected $auth;
+
+    public function __construct(AuthService $auth)
+    {
+        $this->auth = $auth;
+    }
     //Login Controller
     public function index(Request $request)
     {
-      $credentials = request(['email', 'password']);
-      if (!$token = auth()->attempt($credentials)) {
-          return  response()->json([
-              'errors' => [
-                  'msg' => ['Incorrect username or password.']
-              ]
-          ], 401);
-      }
-
-      $user = Auth::user();
-      $tokenResult = $user->createToken('Personal Access Token');
-      $token = $tokenResult->accessToken;
-
-      return \response()->json([
-        'success' => true,
-        'token'   => $token,
-        // 'user'    => $user
-      ], 200);
+        return $this->auth->Login($request);
     }
 
     // profile
     public function profile()
     {
-      $user = Auth::user();
-      return response()->json([
-        'user'  => $user
-      ]);
+      return $this->auth->Profile();
     }
 
     // Logout
     public function logout(Request $request)
     {
-      if (Auth::user()) {
-        $user = Auth::user()->token();
-        $user->revoke();
-
-        $ActivityLog = ActivityLog::create([
-          'user_id' => Auth::id(),
-          'method'  => 'LogOut',
-          'Note'    => 'Logout successfully'
-        ]);
-
-        return response()->json([
-          'success' => true,
-          'message' => 'Logout successfully',
-          'log'     => $ActivityLog
-        ],200);
-      }else {
-        return response()->json([
-          'success' => false,
-          'message' => 'Unable to Logout'
-        ],400);
-      }
+      return $this->auth->Logout($request);
     }
 }
